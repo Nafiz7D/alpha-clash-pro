2f3970050a608cfbb8e614981d21a865d6e2f1d3
Nafiz7D <mrnafsu07@gmail.com>
2024-02-14 22:05:54 +0600

    added the final part

---
+++ scripts/utility.js
@@ -0,0 +1,44 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden')
+}
+
+function showElementByID(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
+function getARandomAlphabet (){
+    const alphabetString =  'abcdefghizklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+// function getARandomAlphabet(){
+//     // get oe create an alphabet array
+    // const alphabetString = 'abcdefghizklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+//     // console.log(alphabets);
+    
+
+//     // get a random index between 0-25
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+    
+
+//     const alphabet = alphabets [index];
+//     // console.log(index, alphabet);
+    
+//     return alphabet;
+// }
